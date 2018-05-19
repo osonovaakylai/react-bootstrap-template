7cbdf0a54b32880c4af4accac21918d78e087a8c
osonovaakylai <osonovaakylai@gmail.com>
2018-05-19 12:51:08 +0600

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,83 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-
-class App extends Component {
+// import Home from './components/Home';
+import PropTypes from 'prop-types';
+class AddFriend extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      newFriend: ""
+    }
+    this.updateNewFriend = this.updateNewFriend.bind(this);
+    this.handleAddNew = this.handleAddNew.bind(this);
+  }
+  updateNewFriend(e) {
+    this.setState({
+      newFriend: e.target.value
+    })
+  }
+  handleAddNew(){
+    this.props.addNew(this.state.newFriend)
+    this.setState({
+      newFriend: ""
+    })
+  }
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </div>
-      </Router>
+      <div>
+        <input 
+          type="text"
+          value={this.state.newFriend}
+          onChange={this.updateNewFriend}
+        />
+        <button onClick={this.handleAddNew}>Add new friend</button>
+      </div>
+    )
+  }
+}
+AddFriend.propTypes = {
+  addNew: PropTypes.func.isRequired
+}
+
+class ShowList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.names.map((friend) => <li>{friend}</li>)}
+      </ul>
     );
+  }
+
+}
+ShowList.defaultProps = {
+  names: []
+}
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: "Akylai",
+      friends: ["Begi", "Atay", "Arlen", "Kilich"]
+    }
+    this.addFriend = this.addFriend.bind(this)
+  }
+
+  addFriend(friend) {
+    this.setState((state) => ({
+      friends: state.friends.concat([friend])
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My name is {this.state.name}</h1>
+        <AddFriend addNew={this.addFriend} />
+        <ShowList names={this.state.friends} />
+      </div>
+
+    )
   }
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './home.css';
 export default class Home extends Component {
+   
    render() {
       return (
-         <Grid>
-            <Jumbotron>
-               <h2>Heyyyyy</h2>
-               <p>How are you? :)</p>
-            </Jumbotron>
-            <Link to="/about">
-            <Button bsStyle="primary">About </Button>
-            </Link>
-         </Grid>
+         <div>
+             
+         </div>
+         
       )
    }
 }
